12b0c0ec571d044008c3fe0d10d7d0c97df7a0aa
stephen <step5790@stud.kea.dk>
2023-07-02 21:57:19 +0200

    added body-parser

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, function () {
   console.log("server started");
